be0df46ec9b1bc0ef69a968133b77d82b1cf3471
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-02-23 15:05:30 +0100

    Corrected certain associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,18 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, :list, presence: true
+  validates_uniqueness_of :movie_id, scope: [:list_id]
+  validates :comment, length: { minimum: 6 }
+
+  # before_destroy :check_movie_presence
+
+  # private
+
+  # def check_movie_presence
+  #   if movie.bookmarks.count > 1
+  #     raise ActiveRecord::InvalidForeignKey
+  #   end
+  # end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks # dependent: :restrict_with_exception
+  has_many :lists, through: :bookmarks
+
+  validates :title, :overview, presence: true
+  validates_uniqueness_of :title
 end
