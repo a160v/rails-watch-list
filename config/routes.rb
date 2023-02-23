5de2883809951714ef8328d820b2ca0d8af30536
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-02-23 16:08:39 +0100

    Seed db with API & created list controller + view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  # get 'lists/create'
+  get 'lists', to: 'lists#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+2.times do |page| # Run the command for 10 times
+  url = "https://api.themoviedb.org/3/movie/popular?api_key=b372057c7f9943482556d6e5ca9cc046&language=en-US&page=#{page+1}" # Replace YOUR_API_KEY with your actual API key
+  movies_serialized = URI.open(url).read
+  movies = JSON.parse(movies_serialized)['results']
+
+  movies.each do |movie|
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      rating: movie['vote_average'],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
+    )
+    puts "Created #{movie['title']}"
+  end
+end
